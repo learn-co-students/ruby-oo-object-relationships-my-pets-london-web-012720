5183f04c0a76d15b252bc461f6ad41cbc423ff1c
github username <github email address>
2020-02-04 17:57:54 +0000

    Done.

---
+++ lib/console.rb
@@ -0,0 +1,22 @@
+require_relative 'cat.rb'
+require_relative 'dog.rb'
+require_relative 'owner.rb'
+
+require "pry"
+
+
+# o1 = Owner.new("Dave")
+# o2 = Owner.new("Mick")
+# o3 = Owner.new("Pete")
+
+# c1 = Cat.new("Cat1", o1)
+# c2 = Cat.new("Cat2", o2)
+# c3 = Cat.new("Cat3", o3)
+
+# d1 = Dog.new("Dog1", o1)
+# d2 = Dog.new("Dog2", o2)
+# d3 = Dog.new("Dog3", o3)
+
+
+
+# binding.pry
